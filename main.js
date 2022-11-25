f981e0ae467b422f2a602225123b85134d0256f0
Yassin Hammami <yas.hammami@gmail.com>
2022-11-25 16:38:14 +0100

    Je viens de contrôler la reception de l'api

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const apikey = "c5f20d8ebc0a849662963132a0c09343"
 
-let url1=`https://api.openweathermap.org/data/2.5/weather?q=Marseille&appid=${apikey}`
+let url1=`https://api.openweathermap.org/data/2.5/weather?q=Marseille&appid=${apikey}&units=metric&lang=fr`
 
-fetch(url1).then(response => response.json())
+fetch(url1).then(response => response.json().then(data => console.log(data)))
