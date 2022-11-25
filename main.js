a987c7238c50881f60a83011312975e2d55880e4
Yassin Hammami <yas.hammami@gmail.com>
2022-11-25 17:45:57 +0100

    J'ai crée l'appli elle marche

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const apikey = "c5f20d8ebc0a849662963132a0c09343"
 
-let url1=`https://api.openweathermap.org/data/2.5/weather?q=Marseille&appid=${apikey}&units=metric&lang=fr`
+let apiCall = function (city) {
+    let url1=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric&lang=fr`
 
 fetch(url1).then((response) => response.json().then((data) => {
     console.log(data);
@@ -10,5 +11,14 @@ document.querySelector("#temp").innerHTML ="<i class='fa-solid fa-temperature-th
 document.querySelector("#humidity").innerHTML = "<i class='fa-solid fa-droplet'></i>" + data.main.humidity ;
 
 document.querySelector("#wind").innerHTML ="<i class='fa-solid fa-wind'></i>" + data.wind.speed + "km/h";
-}))
+})).catch(err => console.log('Erreur : ' + err));
+};
+
+
+document.querySelector('form').addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+let ville = document.querySelector('#inputCity').value;
+apiCall(ville);
+});
 
